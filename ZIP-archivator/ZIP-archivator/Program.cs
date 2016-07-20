9c1b1a70548eeb3872bbdf52523ff587cb213652
ViktorKinzerskyi <vi.kinzerskyi@gmail.com>
2016-07-20 22:40:43 +0300

    first comment

--- ZIP-archivator/ZIP-archivator/Program.cs
+++ ZIP-archivator/ZIP-archivator/Program.cs
@@ -17,6 +17,9 @@ namespace ZIP_archivator
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            //Viktor King
+
         }
     }
 }
