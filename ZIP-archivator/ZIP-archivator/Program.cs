0ea961fe0de25d467bcd4f310b88ca804f15796d
dextrous2010 <dextrous2010@gmail.com>
2016-07-21 23:02:38 +0300

    The solution was cleabed and rebuilded

--- ZIP-archivator/ZIP-archivator/Program.cs
+++ ZIP-archivator/ZIP-archivator/Program.cs
@@ -16,14 +16,7 @@ namespace ZIP_archivator
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-
-            
+            Application.Run(new Form1());      
         }
-
-        static void sdf()
-            {
-            
-            }
     }
 }
